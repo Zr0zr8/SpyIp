fe3f8107392c99e880cbe0f6b05640c75c075207
Zr0zr8 <162935805+Zr0zr8@users.noreply.github.com>
2024-07-07 05:27:35 +0300

    Update inject_payload.py

--- app/inject_payload.py
+++ app/inject_payload.py
@@ -1,11 +1,8 @@
 from stegano import lsb
 
-# مسار الصورة الأصلية
-input_image = "app/static/images/original.jpg"
-# مسار الصورة الناتجة بعد الحقن
-output_image = "app/static/images/payload_image.png"
-# محتوى الملف التنفيذي
-payload_content = open("dist/payload", "rb").read()
-
-# حقن الملف التنفيذي داخل الصورة
-lsb.hide(input_image, payload_content.decode('latin-1')).save(output_image)
+def inject_payload(input_image, output_image, payload_file):
+    with open(payload_file, "rb") as f:
+        payload_content = f.read()
+    
+    lsb.hide(input_image, payload_content.decode('latin-1')).save(output_image)
+    return output_image
