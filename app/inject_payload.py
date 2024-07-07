5ec70ad8e72c8fd179f401ab1e06db932df0ec37
Zr0zr8 <162935805+Zr0zr8@users.noreply.github.com>
2024-07-07 06:05:04 +0300

    Update inject_payload.py

--- app/inject_payload.py
+++ app/inject_payload.py
@@ -4,5 +4,8 @@ def inject_payload(input_image, output_image, payload_file):
     with open(payload_file, "rb") as f:
         payload_content = f.read()
     
-    lsb.hide(input_image, payload_content.decode('latin-1')).save(output_image)
-    return output_image
+    # حقن البايلود في الصورة وحفظها
+    encoded_image = lsb.hide(input_image, payload_content.decode('latin-1'))
+    encoded_image.save(output_image)
+    
+    return output_image, "https://spyip.onrender.com/" + output_image.split('/')[-1]
